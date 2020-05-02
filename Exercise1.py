ceac13bca1a476b21ba68314ea150006ebd55114
Kirill Pyzhyanov <kpyzhyanov@yahoo.com>
2020-05-02 13:00:20 +0200

    MAX and MIN methods implemented

--- Exercise1.py
+++ Exercise1.py
@@ -3,7 +3,6 @@ import pandas as pd
 # Create a class for calculating aggregation statistic for a list of items and test it
 
 
-
 test_set = [
     {
         'name': 'Kirill',
@@ -17,12 +16,12 @@ test_set = [
     },
     {
         'name': 'Solo',
-        'salary': 0,
+        'salary': -10000,
         'weekly-sex': 100
     },
     {
         'name': 'Ruzek',
-        'salary': 0,
+        'salary': -200000,
         'weekly-sex': 0
     },
     {
@@ -53,6 +52,19 @@ class StatisticsHelper:
         result = result / len(self.data)
         return result
 
+    def max(self, column_name):
+        result = self.data[0][column_name]
+        for item in self.data:
+            if item[column_name] > result:
+                result = item[column_name]
+        return result
+
+    def min(self, column_name):
+        result = self.data[0][column_name]
+        for item in self.data:
+            if item[column_name] < result:
+                result = item[column_name]
+        return result
 
 
 #Here we test the code
@@ -64,6 +76,8 @@ statistics_helper.print_data()
 column_name = 'salary'
 print('Sum by {} = {}'.format(column_name, statistics_helper.sum(column_name)))
 print('Avg by {} = {}'.format(column_name, statistics_helper.avg(column_name)))
+print('Max by {} = {}'.format(column_name, statistics_helper.max(column_name)))
+print('Min by {} = {}'.format(column_name, statistics_helper.min(column_name)))
 
 
 
