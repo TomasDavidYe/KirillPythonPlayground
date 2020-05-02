6134f4892586692a6479c14075cc05db79b4abf4
Tomas Ye <tomaye@amazon.com>
2020-05-02 12:04:46 +0200

    Adding first exercise for kirill

--- Exercise1.py
+++ Exercise1.py
@@ -1,4 +1,63 @@
 import pandas as pd
-prdel = 'prdel'
-prdel2 = 'prdel2'
+
+# Create a class for calculating aggregation statistic for a list of items and test it
+
+
+test_set = [
+    {
+        'name': 'Kirill',
+        'salary': 100000,
+        'weekly-sex': 10
+    },
+    {
+        'name': 'Tommy',
+        'salary': 200000,
+        'weekly-sex': 0
+    },
+    {
+        'name': 'Solo',
+        'salary': 0,
+        'weekly-sex': 100
+    },
+    {
+        'name': 'Ruzek',
+        'salary': 0,
+        'weekly-sex': 0
+    },
+    {
+        'name': 'Johny',
+        'salary': 50000,
+        'weekly-sex': 12
+    }
+
+]
+
+
+class StatisticsHelper:
+    def __init__(self, data):
+        self.data = data
+
+    def sum(self, column_name):
+        result = 0
+        for item in self.data:
+            result += item[column_name]
+        return result
+
+    def print_data(self):
+        for item in self.data:
+            print(item)
+
+
+#Here we test the code
+print('Running the test')
+
+
+statistics_helper = StatisticsHelper(test_set)
+statistics_helper.print_data()
+column_name = 'salary'
+print('Sum by {} = {}'.format(column_name, statistics_helper.sum(column_name)))
+
+
+#TODO
+# Implement avg, min, max,
 
