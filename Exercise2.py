f09a27fbf2990e6752042e18d000fc41f3a2ee30
Tomas Ye <tomaye@amazon.com>
2020-05-03 14:08:52 +0200

    [EXERCISE 2] TODO 1 -> Implement a sorting algorithm

---
+++ Exercise2.py
@@ -0,0 +1,4 @@
+#TODO 1
+#Create a new class and put there a method for sorting lists of numbers in ascending order
+# Example 1) [4, 1, 5, 2] -> [1, 2, 4, 5],
+# Example 2) [3, 1, 1, 2] -> [1, 1, 2, 3]
