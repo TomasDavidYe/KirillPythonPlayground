1b4c1f7e46a4af9b860cf14755fafb595efe3180
Kirill Pyzhyanov <kpyzhyanov@yahoo.com>
2020-05-03 14:00:07 +0200

    Added person_max/min methods and find_higher_than method

--- Exercise1.py
+++ Exercise1.py
@@ -66,6 +66,28 @@ class StatisticsHelper:
                 result = item[column_name]
         return result
 
+    def person_min(self, column_name):
+        result = self.data[0]
+        for item in self.data:
+            if item[column_name] < result[column_name]:
+                result = item
+        return result['name']
+
+    def person_max(self, column_name):
+        result = self.data[0]
+        for item in self.data:
+            if item[column_name] > result[column_name]:
+                result = item
+        return result['name']
+
+    def find_higher_than(self, column_name, value):
+        result = []
+        for item in self.data:
+            if item[column_name] > value:
+                result.append(item)
+        return result
+
+
 
 #Here we test the code
 print('Running the test')
@@ -74,10 +96,14 @@ print('Running the test')
 statistics_helper = StatisticsHelper(test_set)
 statistics_helper.print_data()
 column_name = 'salary'
+value = 10000
 print('Sum by {} = {}'.format(column_name, statistics_helper.sum(column_name)))
 print('Avg by {} = {}'.format(column_name, statistics_helper.avg(column_name)))
 print('Max by {} = {}'.format(column_name, statistics_helper.max(column_name)))
 print('Min by {} = {}'.format(column_name, statistics_helper.min(column_name)))
+print('Person with Min by {} = {}'.format(column_name, statistics_helper.person_min(column_name)))
+print('Person with Max by {} = {}'.format(column_name, statistics_helper.person_max(column_name)))
+print('Person with {} higher than {} = {}'.format(column_name, value, statistics_helper.find_higher_than(column_name,value)))
 
 
 
@@ -86,4 +112,8 @@ print('Min by {} = {}'.format(column_name, statistics_helper.min(column_name)))
 
 #TODO 2
 # Implement a function which, given a column name, returns a name of the person with the min / max value
+
+#TODO 3
+# Given column name and a value return a subset of the test collection such that each item in the subcollection has its value
+# higher than the given value
 
