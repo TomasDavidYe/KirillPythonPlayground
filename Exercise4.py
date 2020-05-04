7b152138aeaee388f2a9f366a59102040f402845
Tomas Ye <tomaye@amazon.com>
2020-05-05 00:27:07 +0200

    [EXERCISE 4] A chess board

---
+++ Exercise4.py
@@ -0,0 +1,8 @@
+# Create a class ChessBoard which has a filed n = dimension of the board
+# Let 'O' character represent an empty field
+# Let 'X' character represent an blocked field
+# The ChessBoard, after initialization should have all fields empty
+# The chess board should implment a method block(x, y) to block a field with coordinates (x, y)
+# The chess board should implment a method print() to display the board on the console
+
+
